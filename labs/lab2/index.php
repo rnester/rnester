76b9e31844fcb5101d698cde51472906608162da
rnester <rnester02@gmail.com>
2017-09-09 00:13:08 +0000

    lab 2

---
+++ labs/lab2/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>777 Slot Machine</title>
+        <link  href="css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+    
+    <?php
+    include 'functions.php';
+    ?>
+
+    <body>
+
+    <div id="main">
+        
+        <?php
+            play();
+        ?>
+        
+        <form>
+            <input type="submit" value="SPIN!"/>
+        </form>
+        
+    </div>
+
+
+    </body>
+</html>
